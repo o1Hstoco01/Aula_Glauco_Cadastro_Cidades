fd20815d38b8db1b1503ac3ad3b7ea5179a9f033
sophia.caires <henristoco@gmail.com>
2024-10-07 16:53:38 -0300

    criando o componente cidade, cidade service e http no constructor

---
+++ src/app/cidade/cidade.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cidade',
+  templateUrl: './cidade.component.html',
+  styleUrl: './cidade.component.css'
+})
+export class CidadeComponent implements OnInit{
+
+  ngOnInit(): void {
+   
+  }
+
+}
