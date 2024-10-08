632f2e89ab52fc2d524b61684b3c076c9d76ae3d
Henrique <henristoco@gmail.com>
2024-10-08 09:43:19 -0300

    criando cidade.ts e adicionando uma cidade de teste

---
+++ src/app/cidade.ts
@@ -0,0 +1,7 @@
+export interface Cidade {
+    CEP: number,
+    Nome: string,
+    Estado: string,
+    Ddd: number,
+    Populacao: number
+}
--- src/app/cidade/cidade.component.ts
+++ src/app/cidade/cidade.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cidade } from '../cidade';
+
 
 @Component({
   selector: 'app-cidade',
@@ -6,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './cidade.component.css'
 })
 export class CidadeComponent implements OnInit{
+  cidades: Cidade  [] = [
+    {
+    "CEP":15000,
+    "Nome":"Cidade",
+    "Estado": "SP",
+    "Ddd":185030.00,
+    "Populacao": 30000
+    },
+   ];
 
   ngOnInit(): void {
    
